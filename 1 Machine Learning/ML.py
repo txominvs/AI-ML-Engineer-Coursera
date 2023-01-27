89bb315468fad3a2c4f8a70c0d8016d47b788f3c
Txomin V S <txominvs@gmail.com>
2023-01-27 09:39:57 +0100

    Add Linear Regression

--- 1 Machine Learning/ML.py
+++ 1 Machine Learning/ML.py
@@ -1,7 +1,15 @@
+#Until 38
 # SKILLS: Regression Classification Clustering Scikit learn Scipy
 
 # SUPERVISED LEARNING
-# - Regression/Estimation: continous values
+# - Regression/Estimation: continous values (simple regression 1 feature, multiple more features)
+#       >> Ordinal, poisson, fast forest quantile, linear (analytic formulas!),
+#       >> Polynomial, Lasso, Stepwise, ridge, bayesian linear, Neural Network,
+#       >> Decision forest, boosted decision tree, K-Nearest Neighbors
+#       ++ K-fold cross-validation (multiple train/test split and average accuracy)
+#       ++ Metrics: MAE, MSE, RMSE, Relative Absolute Error (prediction error/mean value predictor)
+#                   Relative Squared Error(prediction error squared/mean value deviation squared)
+#                   R² metric = 1 - Relative Squared Error, the higher the better
 # - Classification
 # UNSUPERVISED LEARNING (less controled, fewer methods)
 # - Dimension reduction/Feature reduction: Principal Component Analysis
@@ -20,3 +28,10 @@ sklearn.model_selection.train_test_split(X, Y, test_size = 0.3)
 model = sklearn.svm.SVC(gamma=0.001, C=100.) .fit(x, y) .predict(x_test)
 print(sklearn.metrics.confusion_matrix(y_test, yhat, labels=[1,0]))
 pickle.dumps(model)
+
+dataframe = pandas.read_csv(); dataframe.head(); dataframe.describe();
+dataframe[["column1","columname2"]].hist(); plt.show();
+mask = np.random.rand(len(dataframe)) < 0.8; train, test = dataframe[mask], dataframe[~mask]
+
+regr = sklearn.linear_model.LinearRegression(); regr.fit(x,y); n,m = regr.intercept_, regr.coef_
+regr.predict(x); sklearn.metrics.r2_score(x,y) # ranges from [-inf worse, +1 best]
