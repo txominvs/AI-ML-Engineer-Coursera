23a4f81e0e6a2e280884c930386cd7db7ac41c26
Txomin <Txomins email>
2023-02-14 16:34:41 +0100

    Add keras classification

--- 2 Keras Deep Learning/keras_cheatsheet.py
+++ 2 Keras Deep Learning/keras_cheatsheet.py
@@ -4,20 +4,42 @@ dataframe.isnull().sum()
 features = dataframe[dataframe.columns[dataframe.columns != 'remove_this_column']]; target = dataframe['column_name']
 normalized_dataframe = (dataframe - dataframe.mean()) / dataframe.std()
 
+
+model.save('classification_model.h5')
+model = keras.models.load_model('classification_model.h5')
+
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+matplotlib.pyplot.imshow(X_train[0])
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2]).astype('float32') # flatten
+x_train = x_train / 255 # normalize
+
+
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(50, activation='relu', input_shape=(number_of_features,))) # number_of_features = features_norm.shape[1]
+model.add(keras.layers.Dense(50, activation='relu'))
+
+
 ##############
 # REGRESSION #
 ##############
 
-from keras.models import Sequential
-from keras.layers import Dense
+model.add(keras.layers.Dense(1)) # regression -> no loss
 
-# create model
-model = Sequential()
-model.add(Dense(50, activation='relu', input_shape=(number_of_features,))) # number_of_features = features_norm.shape[1]
-model.add(Dense(50, activation='relu'))
-model.add(Dense(1))
-
-# compile model
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
 
 model.fit(features_norm, target, validation_split=0.3, epochs=100, verbose=2)
+
+##################
+# CLASSIFICATION #
+##################
+
+model.add(keras.layers.Dense(num_classes, activation='softmax')) # classification -> softmax
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+y_train = keras.utils.to_categorical(y_train) # one-hot encode outputs
+
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, verbose=2)
+
+loss_value, *metrics_values = model.evaluate(x_test, y_test, verbose=0)
