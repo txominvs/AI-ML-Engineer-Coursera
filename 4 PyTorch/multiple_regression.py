9e16ac5120fa65dd4b32f427ac090f1fc89f63b0
Txomin <Txomins email>
2023-03-30 11:27:41 +0200

    Finish linear regression

--- 4 PyTorch/multiple_regression.py
+++ 4 PyTorch/multiple_regression.py
@@ -1,6 +1,10 @@
 # Multiple linear regression:
 # y = b + x_i w_i
 #   = matrix[x_i inputs as rows] * column_matrix[w_i] + column_matrix[b b b b]
+# Multiple outputs:
+# [y0 y1] = [b0 b1] + [features as a row] * [column of weights 1, column of weights 2]
+# outputs for different samples stacked as rows = """""""" + [features stacked as rows] * """"""
+# rows = sample; columns = feature
 
 import torch
 from torch import nn
@@ -24,14 +28,14 @@ def forward(x):
 yhat = forward(x)
 
 ### Object oriented approach
-class linear_regression(nn.Module):
+class Linear_regression(nn.Module):
     def __init__(self, input_size, output_size):
         super().__init__()
         self.linear = nn.Linear(input_size, output_size)
     def forward(self, x):
         return self.linear(x)
-yhat = model(x)
-parameters = list(model.parameters()); parameters = model.state_dict()
+model = Linear_regression(input_size=2, output_size=3)
+yhat = model(x); parameters = list(model.parameters()); parameters = model.state_dict()
 
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 criterion = nn.MSELoss()
@@ -46,3 +50,20 @@ for epoch in range(100):
         optimizer.step()
 
 validation_loss = criterion(model(x_valdation), y_validation)
+
+def export_model():
+    model = Linear_regression(3,10)
+    samples = torch.tensor([
+        [1.,2.,3.], # first sample
+        [4.,5.,16.], # second sample
+    ])
+
+    weights = list(model.parameters())[0]
+    biases  = list(model.parameters())[1]
+
+    result = torch.mm(samples, weights.transpose(0,1))
+    for index, row in enumerate(result):
+        result[index] = row + biases
+
+    print(result)
+    print(model(samples))
