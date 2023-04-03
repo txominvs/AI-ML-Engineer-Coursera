b9cdb834faabb5b89fdb5d9b012efafca2b11c25
Txomin <Txomins email>
2023-04-03 12:05:21 +0200

    Add logistic regression classify, log loss entropy

--- 4 PyTorch/pytorch_classification.py
+++ 4 PyTorch/pytorch_classification.py
@@ -1,5 +1,15 @@
 threshold_function = lambda x: 1 if x>0 else 0
 sigmoid = lambda x: 1/(1+exp(-x))
+# bernulli_distribution = probability of sequence of events = P(a)^Na * P(b)^Nb
+# Likelihood = P(model correct | having seen a sequence of events)
+#            = P(sequence of events | using this model) = x^number_of_tails * (1-x)^number_of_heads
+log_loss = log(likelihood) / all_tries = number_of_tails/all_tries*log(x) + number_of_heads/all_tries*log(1-x)
+# This means log_loss=entropy since probability=exp(sequence_length*entropy)
+
+###
+# This might be the reason why we minimize LOG_LOSS in Classification instead of MEAN_SQUARED_ERROR
+# We try to minimize LOG_LOSS = maximize LIKELIHOOD = maximize P(model is correct | having seen a sequence of training samples)
+###
 
 ###
 # CLASSIFICATION
@@ -35,3 +45,31 @@ x = torch.tensor([ # batch size = 3
 ])
 yhat = model(x)
 parameters = list(model.parameters()); parameters = model.state_dict()
+
+criterion = nn.MSELoss() # = lambda yhat, y: torch.mean((yhat-y)**2)
+criterion = nn.BCELoss() # = lambda yhat, y: -1 * torch.mean( y*torch.log(yhat) + (1-y)*torch.log(1-yhat) )
+
+### Training example
+
+from torch.utils.data import Dataset, DataLoader
+
+class Custom_data(Dataset):
+    def __init__(self):
+        self.x, self.y, self.len = ...
+    def __getitem__(self, index):      
+        return self.x[index], self.y[index]
+    def __len__(self):
+        return self.len
+
+trainloader = DataLoader(dataset=Custom_data(), batch_size=3)
+criterion = nn.BCELoss() # for regressions instead = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=2.0)
+
+for epoch in range(epochs):
+    for x, y in trainloader: 
+        yhat = model(x)
+        loss = criterion(yhat, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        predicted_labels = yhat >= 0.5
