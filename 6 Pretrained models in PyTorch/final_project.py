160bd7bfac216ce39c8a8bb4526885dfb3a48ccd
Txomin V S <txominvs@gmail.com>
2023-05-18 12:54:39 +0200

    Add final project

---
+++ 6 Pretrained models in PyTorch/final_project.py
@@ -0,0 +1,45 @@
+model = models.resnet18(pretrained=True)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+model.fc = nn.Linear(512, 7)
+
+composed = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+train_dataset = Dataset(transform=composed, train=True)
+validation_dataset = Dataset(transform=composed, train=False)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=15)
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=10)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(
+    [param for param in model.parameters() if param.requries_grad],
+    lr=0.003,
+)
+
+for epoch in range(20):
+    epoch_loss = 0
+    for x, y in train_loader:
+        model.train()
+        optimizer.zero_grad()
+        z = model(x)
+        loss = criterion(z, y)
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+
+    correct = 0
+    for x, y in validation_loader:
+        model.eval()
+        z = model(x)
+        _, yhat = torch.max(z.data, 1)
+        correct += (yhat == y).sum().item()
+    epoch_accuracy = correct / len(validation_dataset)
+    
